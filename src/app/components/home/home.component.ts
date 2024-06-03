ac032f537096397102ee4e8d3686056a049c8039
covegi <peter.de.michiel@covegi.com>
2024-06-03 15:26:09 +0200

    added home hero background

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  Renderer2,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('image') image!: ElementRef;
+  renderer = inject(Renderer2);
+
+  ngAfterViewInit() {
+    const img = new Image();
+    img.src = 'assets/images/background.jpg';
+
+    img.onload = () => {
+      if (this.image) {
+        this.renderer.setStyle(this.image.nativeElement, 'src', img.src);
+      }
+    };
+  }
+}
